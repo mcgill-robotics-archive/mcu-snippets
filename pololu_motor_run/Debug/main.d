7b94f3246ebdf6ed453bc86c1e0b2e7a9b48ff77
Ankita0 <ankita.sharma2@mail.mcgill.ca>
2017-01-22 12:54:49 -0500

    Add CCS code for running a motor with Pololu motor controller

---
+++ pololu_motor_run/Debug/main.d
@@ -0,0 +1,19 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
+main.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h
+main.obj: /home/ankita/TI/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h
+main.obj: /home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+main.obj: /home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+main.obj: /home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h
+main.obj: /home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/pwm.h
+
+../main.c: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
+/opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h: 
+/home/ankita/TI/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h: 
+/home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+/home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+/home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h: 
+/home/ankita/TI/TivaWare_C_Series-2.1.3.156/driverlib/pwm.h: 
---
+++ pololu_motor_run/main.c
@@ -0,0 +1,142 @@
+/*******************************************************************************
+ *                             Include Statements                              *
+ ******************************************************************************/
+
+/* C standard integer header
+ * e.g., uint32_t
+ */
+#include <stdint.h>
+
+/* C standard boolean header
+ * e.g., bool (used inside gpio.h, sysctl.h)
+ */
+#include <stdbool.h>
+
+/* Device memory map header
+ * e.g., GPIO_PORTF_BASE
+ */
+#include "inc/hw_memmap.h"
+
+/* GPIO API header
+ * eg., GPIO_PIN_1
+ */
+#include "driverlib/gpio.h"
+
+/* System control driver header
+ * e.g., SysCtlPeripheralEnable, SysCtlPeripheralReady
+ */
+#include "driverlib/sysctl.h"
+
+/* Pin-to-peripheral mapping header
+ * e.g., TODO
+ */
+#include "driverlib/pin_map.h"
+
+/* PWM driver header
+ * e.g., SysCtlPWMClockSet
+ */
+#include "driverlib/pwm.h"
+
+/*******************************************************************************
+ *                                Main Method                                  *
+ ******************************************************************************/
+
+void main(void) {
+
+	// Set system clock to 80MHz using a PLL (200MHz / 2.5 = 80MHz)
+	SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+	                       SYSCTL_XTAL_16MHZ);
+
+	/* Before you begin:
+	 * We want a PWM output to appear on PF3 (the green LED on the LaunchPad).
+	 * If you go to page 1233 in the MCU's data sheet, you'll see that the PWM
+	 * output on PF3 is controlled by PWM Module 1, PWM Generator 3, and PWM
+	 * output 7 (see the diagram on page 1232). These numbers will affect the
+	 * arguments to the macro calls below.
+	 */
+
+	/* Set PWM clock base frequency.
+	 * PWM base frequency is the processor clock frequency divided by X, where
+	 * the input to SysCtlPWMClockSet is SYSCTL_PWMDIV_X.
+	 * If you're using the external oscillator (default), the processor clock
+	 * will be 80MHz. In this case, the divisor is 8, so the frequency base is
+	 * 10MHz. Note that this isn't the actual frequency of the PWM signal. That
+	 * will be set later to be an integer multiple of this value.
+	 */
+	SysCtlPWMClockSet(SYSCTL_PWMDIV_8);
+
+	// Enable the PWM peripheral and wait for it to be ready.
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM1)){}
+
+	// Enable the GPIO peripheral and wait for it to be ready.
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
+	
+	//Enable GPIO ports with base D and wait for it to be ready
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){}
+
+	// Configure the internal multiplexer to connect the PWM peripheral to PF3
+	GPIOPinConfigure(GPIO_PF3_M1PWM7);
+
+	// Set up the PWM module on pin PF3
+	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+
+	// Set up the InA pin as D0, and InB as pin D1
+	//Can also write as:
+	//GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0|GPIO_PIN_1);
+	GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0);
+	GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_1);
+
+	/* Configure PWM mode to count up/down without synchronization
+	 * This is just a detail, you probably don't have to worry about it.
+	 * See pages 1235 and 1237 in the data sheet if you want to learn more.
+	 */
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_UP_DOWN |
+                    PWM_GEN_MODE_NO_SYNC);
+
+    /* Set the PWM period based on the time base we configured with
+     * SysCtlPWMClockSet above.
+     *
+     * If we want a frequency of 250Hz:
+     * N = (1 / f) * BaseFreq, where N is the last parameter in PWMGenPeriodSet,
+     * f is the desired frequency, and BaseFreq is the PWM base frequency set
+     * above (10 MHz in this case). (1 / 250Hz) * 10 MHz = 40000.
+     *
+     * Note that the maximum period you can set is 2^16. If you need more than
+     * that, you'll need to modify the clock divisor.
+     */
+    //From PWM peripheral base 1 to peripheral pin 3 (configured as pwm)
+    //with period as 40000 clock ticks
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 40000);
+
+    /* Set the PWM duty cycle to 25%. The duty cycle is a function of the period.
+     * You can get the period set above using PWMGenPeriodGet.
+     *
+     * Warning! The PWM generator often freaks out when you set 100% duty cycle.
+     * It's a known issue in the silicon:
+     * https://e2e.ti.com/support/microcontrollers/tiva_arm/f/908/t/448664
+     * To avoid this, cap the PWM output at (MaxDuty - 1). I personally wouldn't
+     * trust it at 0% either. If you strictly need 0% and 100%, you should
+     * switch the pin back to a GPIO. Though this may take some time (~5us).
+     */
+    // As muxed from M1PWM7
+    // Duty-cycle of the PWM set as half the period of the period of the PWM signal.
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,
+                         PWMGenPeriodGet(PWM1_BASE, PWM_GEN_3)/2);
+
+    // Enable the PWM output signal
+    PWMOutputState(PWM1_BASE, PWM_OUT_7_BIT, true);
+
+    // Enable the PWM peripheral
+    PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+
+
+    // Pin InA set to high and InB set to low
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0xFF);
+    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x00);
+
+    // Delay indefinitely so the program never stops.
+    while(1){}
+}
