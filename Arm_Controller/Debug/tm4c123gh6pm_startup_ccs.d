789d139b49c1ceeae64547337a973de5116918d2
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2017-01-29 12:10:15 -0500

    Stop tracking project files and compile output

--- Arm_Controller/Debug/tm4c123gh6pm_startup_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
-tm4c123gh6pm_startup_ccs.obj: /home/felix/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
-
-../tm4c123gh6pm_startup_ccs.c: 
-/home/felix/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
--- pololu_motor_run/Debug/tm4c123gh6pm_startup_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
-tm4c123gh6pm_startup_ccs.obj: /opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
-
-../tm4c123gh6pm_startup_ccs.c: 
-/opt/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
