b7f5c50974d94ad50e7f009adc3634bac2200f82
Elsa Riachi <elsa.riachi@mail.mcgill.ca>
2017-02-25 15:46:21 -0500

    update main to work with one isr. second isr does not service

--- 3_mag_encoder_read/main.c
+++ 3_mag_encoder_read/main.c
@@ -32,7 +32,10 @@ void duty_cycle_2(void);
 
 // global variables
 uint32_t sys_clock;
-int32_t  start = 0, end = 0, length = 0;
+int32_t  start_0 = 0, end_0 = 0, length_0 = 0;
+int32_t  start_1 = 0, end_1 = 0, length_1 = 0;
+int32_t  start_2 = 0, end_2 = 0, length_2 = 0;
+
 uint32_t angle0, angle1, angle2;
 int encoder0_flag, encoder1_flag, encoder2_flag;
 
@@ -45,26 +48,48 @@ int main(void)
     // Configure system clock at 80 MHz.
 	SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
     sys_clock = SysCtlClockGet();
-
-    // Enable the processor to respond to interrupts.
     IntMasterEnable();
+
+//    TimerDisable(WTIMER0_BASE, TIMER_BOTH);
+//    TimerDisable(WTIMER1_BASE, TIMER_BOTH);
+//    TimerDisable(WTIMER2_BASE, TIMER_BOTH);
 
     init_UART();
     init_pwm();
     init_timer();
 
+    IntEnable(INT_WTIMER0B);
+    IntPrioritySet(INT_WTIMER0B, 2);
+
+
+    IntEnable(INT_WTIMER1B);
+    IntPrioritySet(INT_WTIMER1B, 2);
+
+
+//    IntEnable(INT_WTIMER2B);
+//    IntPrioritySet(INT_WTIMER2B, 2);
+
+
     TimerEnable(WTIMER0_BASE, TIMER_BOTH);
     TimerEnable(WTIMER1_BASE, TIMER_BOTH);
-    TimerEnable(WTIMER2_BASE, TIMER_BOTH);
+//    TimerEnable(WTIMER2_BASE, TIMER_BOTH);
+
+
+    // The specified interrupt is enabled in the interrupt controller.
+
+    //IntEnable(INT_WTIMER0B|INT_WTIMER1B|INT_WTIMER2B);
+
+    // Enable the processor to respond to interrupts.
+
 
     while(1) {
+    	if(encoder1_flag==1){
+    		duty_cycle_1();
+    	}
     	if (encoder0_flag==1){
     		duty_cycle_0();
     	}
-    	else if(encoder1_flag==1){
-    		duty_cycle_1();
-    	}
-    	else if(encoder2_flag== 1){
+    	if(encoder2_flag== 1){
     		duty_cycle_2();
     	}
     }
@@ -99,6 +124,7 @@ void init_pwm(void)
 	GPIOPinConfigure(GPIO_PF3_M1PWM7);
 	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
 
+
 	PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_UP_DOWN |
 					PWM_GEN_MODE_NO_SYNC);
 
@@ -115,10 +141,13 @@ void init_timer(void)
 {
     // Enable and configure Timer0 peripheral.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_WTIMER0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_WTIMER0)){}
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_WTIMER1);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_WTIMER1)){}
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_WTIMER2);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_WTIMER2)){}
 
     // Initialize timer A and B to count up in edge time capture mode
     TimerConfigure(WTIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME_UP | TIMER_CFG_B_CAP_TIME_UP));
@@ -137,8 +166,8 @@ void init_timer(void)
     TimerControlEvent(WTIMER2_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
     TimerControlEvent(WTIMER2_BASE, TIMER_B, TIMER_EVENT_NEG_EDGE);
 
-    //TimerPrescaleSet(WTIMER0_BASE, TIMER_A , 0 );
-    //TimerPrescaleSet(WTIMER0_BASE, TIMER_B , 0 );
+    TimerPrescaleSet(WTIMER0_BASE, TIMER_A , 0 );
+    TimerPrescaleSet(WTIMER0_BASE, TIMER_B , 0 );
 
     //set the value that the timers count to (0x9C3F = 39999)
     //CO2 sensor outputs 1khz pwm so with mcu at 40Mhz, timers should stay in sync with CO2 output
@@ -149,6 +178,7 @@ void init_timer(void)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
+    //Timer pins are PC4, PC5; PC6, PC7; PD0, PD1
     GPIOPinConfigure(GPIO_PC4_WT0CCP0);
     GPIOPinTypeTimer(GPIO_PORTC_BASE, GPIO_PIN_4);
     GPIOPinConfigure(GPIO_PC5_WT0CCP1);
@@ -163,7 +193,6 @@ void init_timer(void)
     GPIOPinTypeTimer(GPIO_PORTD_BASE, GPIO_PIN_0);
     GPIOPinConfigure(GPIO_PD1_WT2CCP1);
     GPIOPinTypeTimer(GPIO_PORTD_BASE, GPIO_PIN_1);
-
 
 
     // Registers a interrupt function to be called when timer b hits a neg edge event
@@ -187,44 +216,47 @@ void init_timer(void)
     // Enable the indicated timer interrupt source.
     TimerIntEnable(WTIMER2_BASE, TIMER_CAPB_EVENT);
 
-    // The specified interrupt is enabled in the interrupt controller.
-    IntEnable(INT_WTIMER0B);
-    IntEnable(INT_WTIMER1B);
-    IntEnable(INT_WTIMER2B);
 }
+
 
 void
 interrupt_h_encoder_0(void){
     TimerIntClear(WTIMER0_BASE, TIMER_CAPB_EVENT);
-    start = TimerValueGet(WTIMER0_BASE, TIMER_A);
-    end = TimerValueGet(WTIMER0_BASE, TIMER_B);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2, 1);
+    start_0 = TimerValueGet(WTIMER0_BASE, TIMER_A);
+    end_0 = TimerValueGet(WTIMER0_BASE, TIMER_B);
 	encoder0_flag= 1;
+	UARTprintf("in ISR 0 \n");
+
 }
 
 void
 interrupt_h_encoder_1(void){
     TimerIntClear(WTIMER1_BASE, TIMER_CAPB_EVENT);
-    start = TimerValueGet(WTIMER1_BASE, TIMER_A);
-    end = TimerValueGet(WTIMER1_BASE, TIMER_B);
+    start_1 = TimerValueGet(WTIMER1_BASE, TIMER_A);
+    end_1 = TimerValueGet(WTIMER1_BASE, TIMER_B);
 	encoder1_flag= 1;
+	UARTprintf("in ISR 1 \n");
 }
 
 void
 interrupt_h_encoder_2(void){
     TimerIntClear(WTIMER2_BASE, TIMER_CAPB_EVENT);
-    start = TimerValueGet(WTIMER2_BASE, TIMER_A);
-    end = TimerValueGet(WTIMER2_BASE, TIMER_B);
+    start_2 = TimerValueGet(WTIMER2_BASE, TIMER_A);
+    end_2 = TimerValueGet(WTIMER2_BASE, TIMER_B);
 	encoder2_flag= 1;
 }
 
 //When negative edge is hit, record the values and find the difference, output to putty
 void duty_cycle_0(void)
 {
+	//GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2, 2);
 	encoder0_flag= 0;
-    length = end - start;
-    if (length<0){
-    	//UARTprintf("\nLENGTH0 = %d\n", length);
-    	length= 4000+length;
+    length_0 = end_0 - start_0;
+    //UARTprintf("\nLENGTH0 = %d\n", length_0);
+    if (length_0<0){
+
+    	length_0 = 4000+length_0;
     }
     //notes: 10MHZ clock, duty cycle of 500 pwm clock cycles => length is always 0.00005 seconds
     //length = 4001 => 4001 sysclock ticks => 4000 * 1/80M = 0.5 E-4 , length read by timer matches PWM duty cycle
@@ -232,21 +264,24 @@ void duty_cycle_0(void)
 
     //since PWM period is 10000 PWM clock cycles, and length is given in system clock cycles
     //PWM subdiv = 8
-    angle0 = (360*length)/80000;
-    UARTprintf("\nSTART0 = %d\n", start);
-    UARTprintf("\nEND0 = %d\n", end);
-    UARTprintf("\nLENGTH0 = %d\n", length);
-    UARTprintf("\nANGLE0 = %d\n", angle0);
+    angle0 = (360*length_0)/80000;
+
+
+//    UARTprintf("\nSTART0 = %d\n", start);
+//    UARTprintf("\nEND0 = %d\n", end);
+//    UARTprintf("\nLENGTH0 = %d\n", length);
+//    UARTprintf("\nANGLE0 = %d\n", angle0);
 }
 
 void duty_cycle_1(void)
 {
+	//GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 4);
 	encoder1_flag=0;
 
-    length = end - start;
-    if (length<0){
-    	UARTprintf("\nLENGTH1 = %d\n", length);
-    	length= 4000+length;
+    length_1 = end_1 - start_1;
+    if (length_1<0){
+//    	UARTprintf("\nLENGTH1 = %d\n", length_1);
+    	length_1= 4000+length_1;
     }
     //notes: 10MHZ clock, duty cycle of 500 pwm clock cycles => length is always 0.00005 seconds
     //length = 4001 => 4001 sysclock ticks => 4000 * 1/80M = 0.5 E-4 , length read by timer matches PWM duty cycle
@@ -254,21 +289,22 @@ void duty_cycle_1(void)
 
     //since PWM period is 10000 PWM clock cycles, and length is given in system clock cycles
     //PWM subdiv = 8
-    angle1 = (360*length)/80000;
-    UARTprintf("\nSTART1 = %d\n", start);
-    UARTprintf("\nEND1 = %d\n", end);
-    UARTprintf("\nLENGTH1 = %d\n", length);
-    UARTprintf("\nANGLE1 = %d\n", angle1);
+    angle1 = (360*length_1)/80000;
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2, 1);
+//    UARTprintf("\nSTART1 = %d\n", start);
+//    UARTprintf("\nEND1 = %d\n", end);
+//    UARTprintf("\nLENGTH1 = %d\n", length);
+//    UARTprintf("\nANGLE1 = %d\n", angle1);
 }
 
 void duty_cycle_2(void)
 {
 	encoder2_flag=0;
 
-    length = end - start;
-    if (length<0){
-    	UARTprintf("\nLENGTH2 = %d\n", length);
-    	length= 4000+length;
+    length_2 = end_2 - start_2;
+    if (length_2<0){
+//    	UARTprintf("\nLENGTH2 = %d\n", length_2);
+    	length_2= 4000+length_2;
     }
     //notes: 10MHZ clock, duty cycle of 500 pwm clock cycles => length is always 0.00005 seconds
     //length = 4001 => 4001 sysclock ticks => 4000 * 1/80M = 0.5 E-4 , length read by timer matches PWM duty cycle
@@ -276,9 +312,10 @@ void duty_cycle_2(void)
 
     //since PWM period is 10000 PWM clock cycles, and length is given in system clock cycles
     //PWM subdiv = 8
-    angle2 = (360*length)/80000;
-    UARTprintf("\nSTART2 = %d\n", start);
-    UARTprintf("\nEND2 = %d\n", end);
-    UARTprintf("\nLENGTH2 = %d\n", length);
-    UARTprintf("\nANGLE2 = %d\n", angle2);
+    angle2 = (360*length_2)/80000;
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2, 2);
+//    UARTprintf("\nSTART2 = %d\n", start);
+//    UARTprintf("\nEND2 = %d\n", end);
+//    UARTprintf("\nLENGTH2 = %d\n", length);
+//    UARTprintf("\nANGLE2 = %d\n", angle2);
 }
